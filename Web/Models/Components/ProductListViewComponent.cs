c8e83c286a6a834c49c87845af3a4cf644246aab
ahmad zaid <agzaid00@gmail.com>
2022-10-25 15:59:16 +0200

    added product list view component

---
+++ Web/Models/Components/ProductListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Shop;
+
+namespace Web.Models.Components
+{
+    public class ProductListViewComponent :ViewComponent
+    {
+        private readonly IProductService _productService;
+
+        public ProductListViewComponent(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = _productService.GetMany(s => true, null).ToList();
+            return View(model);
+        }
+    }
+}
